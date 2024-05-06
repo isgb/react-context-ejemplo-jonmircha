76612ca1011eb4937375e4a03458dfc6c88e7818
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-05 23:39:27 -0600

    debug

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
+const initialTheme = "light";
+
 export const MyPage = () => {
+   const [theme,setTheme] = useState(initialTheme);
   return (
     <div className="my-page">
-      <Header />
-      <Main />
-      <Footer />
+      <Header theme={theme}/>
+      <Main theme={theme}/>
+      <Footer theme={theme}/>
     </div>
   );
 };
