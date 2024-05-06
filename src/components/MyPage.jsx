0a7cc730f935added68e7484bb997d3f4f7d2d0a
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-05 23:52:42 -0600

    debug

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -3,9 +3,38 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 
 const initialTheme = "light";
+const initialLanguage = "es";
+
+const translations = {
+    es: {
+        headerTitle: "Mi aplicacion SIN Context Api",
+        headerSubtitle: "Mi Cabecera",
+        headerLight: "Claro",
+        headerDark: "Oscuro",
+        buttonLogin: "Iniciar Sesion",
+        buttonLogout: "Cerrar Sesion",
+        mainWelcome: "Bienvenido",
+        mainHello: "Hola usuario",
+        mainContent: "Mi contenido principal",
+        footerTitle: "Mi pie de pagina"
+    },
+    en: {
+            headerTitle: "My application WITHOUT Context Api",
+            headerSubtitle: "My Header",
+            headerLight: "Sure",
+            headerDark: "Dark",
+            buttonLogin: "Login",
+            buttonLogout: "Close Session",
+            mainWelcome: "Welcome",
+            mainHello: "Hello user",
+            mainContent: "My main content",
+            footerTitle: "My footer"
+        }, 
+}
 
 export const MyPage = () => {
    const [theme,setTheme] = useState(initialTheme);
+   const [language, setLanguage] = useState(initialLanguage)
 
    const handleTheme = (e) => {
     console.log(e.target.value);
