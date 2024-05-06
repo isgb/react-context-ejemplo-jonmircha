88a305c0cd6ac98ad1bb19c0ed06eb556eb47c88
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-05 23:48:37 -0600

    debug

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -9,7 +9,12 @@ export const MyPage = () => {
 
    const handleTheme = (e) => {
     console.log(e.target.value);
-   }
+    if(e.target.value === "light"){
+        setTheme("light")
+    }else{
+        setTheme("dark")
+    }
+   };
 
   return (
     <div className="my-page">
