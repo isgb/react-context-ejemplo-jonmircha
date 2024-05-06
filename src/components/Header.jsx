3d60b5aef1d29ef3ad034e2f81c6569fd0e474a7
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-05 23:28:20 -0600

    debug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,12 @@ export const Header = () => {
     <header>
     <h2>Mi aplicación SIN Context API</h2>
     <h3>Mi cabecera</h3>
+    <select name="language">
+        <option value="es">ES</option>
+        <option value="en">EN</option>
+    </select>
+    <input type="radio" name='theme' id="light" value='light'/>
+    <label htmlFor=""></label>
 </header>
   )
 }
