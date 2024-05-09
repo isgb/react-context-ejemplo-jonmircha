344265570d8cd9d0036003b98a3ab6203d296e11
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-08 23:47:51 -0600

    debug

--- src/components/MyPageContext.jsx
+++ src/components/MyPageContext.jsx
@@ -3,7 +3,6 @@ import { HeaderContext } from "./HeaderContext";
 import { FooterContext } from "./FooterContext";
 import { MainContext } from "./MainContext";
 
-const initialTheme = "light";
 const initialLanguage = "es";
 const initialAuth = null;
 
@@ -35,7 +34,6 @@ const translations = {
 }
 
 export const MyPageContext = () => {
-  const [theme, setTheme] = useState(initialTheme);
   const [language, setLanguage] = useState(initialLanguage)
   const [texts, setTexts] = useState(language);
   const [auth, setAuth] = useState(initialAuth)
@@ -43,15 +41,6 @@ export const MyPageContext = () => {
   console.log(texts);
 
   // https://www.youtube.com/watch?v=rg6MPk6uobs  7:50 / 10:38
-
-  const handleTheme = (e) => {
-    // console.log(e.target.value);
-    if (e.target.value === "light") {
-      setTheme("light")
-    } else {
-      setTheme("dark")
-    }
-  };
 
   const handleLanguage = (e) => {
     console.log(e.target.value);
@@ -75,7 +64,7 @@ export const MyPageContext = () => {
 
   return (
     <div className="my-page">
-      <Header
+      <HeaderContext
         theme={theme}
         handleTheme={handleTheme}
         texts={texts}
@@ -83,12 +72,12 @@ export const MyPageContext = () => {
         auth={auth}
         handleAuth={handleAuth}
       />
-      <Main 
+      <MainContext 
         theme={theme} 
         texts={texts} 
         auth={auth}
       />
-      <Footer theme={theme} texts={texts} />
+      <FooterContext theme={theme} texts={texts} />
     </div>
   );
 };
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,6 +1,31 @@
-import { createContext } from "react";
+import { Children, createContext } from "react";
 
-
+const initialTheme = "light";
 const ThemeContext = createContext();
+
+export const ThemeProvider = ({children}) => {
+
+    const [theme, setTheme] = useState(initialTheme);
+    const handleTheme = (e) => {
+        // console.log(e.target.value);
+        if (e.target.value === "light") {
+          setTheme("light")
+        } else {
+          setTheme("dark")
+        }
+      };
+
+    const data = {theme, handleTheme}
+
+    return (
+        <ThemeContext.Provider
+            value={data}
+        >
+            {children}
+        </ThemeContext.Provider>
+    )
+    
+
+}
 
 export default ThemeContext();
